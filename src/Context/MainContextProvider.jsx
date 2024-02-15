f0b795778be79b77dec17aee83ee9b6a0281f4a4
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-15 20:52:04 +0300

    Add ResultIsVisibleContext

--- src/Context/MainContextProvider.jsx
+++ src/Context/MainContextProvider.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import StartIsVisibleContext from "./StartIsVisibleContext";
 import QuestionIsVisibleContext from "./QuestionIsVisibleContext";
 import QuestionIndexContext from "./QuestionIndexContext";
 import ChosenAnswersContext from "./ChosenAnswersContext";
 import ScoreContext from "./ScoreContext";
+import ResultIsVisibleContext from "./ResultIsVisibleContext";
 
 // eslint-disable-next-line react/prop-types
 function MainContextProvider({ children }) {
@@ -42,13 +43,24 @@ function MainContextProvider({ children }) {
         setScore,
     };
 
+    const [resultIsVisible, setResultIsVisible] = useContext(
+        ResultIsVisibleContext
+    );
+
+    const resultIsVisibleState = {
+        resultIsVisible,
+        setResultIsVisible,
+    };
+
     return (
         <StartIsVisibleContext.Provider value={startIsVisibleState}>
             <QuestionIsVisibleContext.Provider value={questionIsVisibleState}>
                 <QuestionIndexContext.Provider value={questionIndexState}>
                     <ChosenAnswersContext.Provider value={chosenAnswersState}>
                         <ScoreContext.Provider value={scoreState}>
-                            {children}
+                            <ResultIsVisibleContext.Provider value={resultIsVisibleState}>
+                                {children}
+                            </ResultIsVisibleContext.Provider>
                         </ScoreContext.Provider>
                     </ChosenAnswersContext.Provider>
                 </QuestionIndexContext.Provider>
