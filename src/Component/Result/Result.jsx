71396c4d219089c335898f6b7a12189d68093cc7
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-15 20:56:53 +0300

    Add component body

--- src/Component/Result/Result.jsx
+++ src/Component/Result/Result.jsx
@@ -1,7 +1,28 @@
-import './Result.style.css'
+import { useContext } from "react";
+import ChosenAnswersContext from "../../Context/ChosenAnswersContext";
+import ResultIsVisibleContext from "../../Context/ResultIsVisibleContext";
+import ScoreContext from "../../Context/ScoreContext";
+import "./Result.style.css";
 
 function Result() {
-    return <></>;
+    const { resultIsVisible } = useContext(ResultIsVisibleContext);
+    const { score } = useContext(ScoreContext);
+    const { chosenAnswers } = useContext(ChosenAnswersContext);
+
+    return (
+        <>
+            {resultIsVisible ? (
+                <div className="result-component">
+                    {chosenAnswers.map((answer) => {
+                        return <div key={answer}>{answer}</div>;
+                    })}
+                    {chosenAnswers.length} sorudan {score} soruyu doğru yaptınız
+                </div>
+            ) : (
+                ""
+            )}
+        </>
+    );
 }
 
 export default Result;
